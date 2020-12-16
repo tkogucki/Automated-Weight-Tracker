ec6cdfed97324c2fc1be78ee59f1058f0faf01ee
tkogucki <bpasssr.71@gmail.com>
2020-12-16 16:19:55 -0600

    Finished data processing and export to csv

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import getpass
 import imaplib
 import email
 import csv
+import re
+from datetime import datetime
 
 # class containing individual email information
 class Email():
@@ -21,6 +23,8 @@ class Email():
         self.date = date
         self.body = ''
         self.html_body = False
+        self.weight = 0
+        self.date_val = None
 
 # Authenticate Function
 def authenticate(file = 'username.txt'):
@@ -40,12 +44,12 @@ def authenticate(file = 'username.txt'):
     _, search_data = mail.search(None, 'ALL')
     return search_data, mail
 
+# returns list of Email classes after sorting through the email data  
 def email_data_extraction(search_data, mail):
     my_message = []
     for num in search_data[0].split():
         email_data = {}
         _, data = mail.fetch(num, '(RFC822)')
-        # print(data[0])
         _, b = data[0]
         email_message = email.message_from_bytes(b)
         # depositing individual properties of email into class
@@ -59,16 +63,39 @@ def email_data_extraction(search_data, mail):
                 curr_email.body = html_body.decode()
                 curr_email.html_body = True
         my_message.append(curr_email)
-    print(my_message)
-    print(my_message[1].body)
-    print(my_message[1].e_from)
-    print(my_message[2].body)
- 
+    return my_message
+
+# filters through strings to find date and weight
+def clean_data(my_messages):
+    pre_string = r'<div dir="auto">'
+    l_pre_string = len(pre_string)
+
+    # use regular expressions to pull weights out of the body string
+    for i in my_messages:
+        x = re.findall(r'\d+\.\d+', i.body)
+        if len(x) == 0:
+            x = re.findall(r'\b\d+\b', i.body)
+        weight = float(x[0])
+        i.weight = weight
+        # use datetime functionality to pull dates out of strings
+        date = datetime.strptime(i.date[5:-15], r'%d %b %Y')
+        i.date_val = date
+#export csv
+def export_csv(my_messages, file_name):
+    with open(file_name, 'w', newline = "") as csvfile:
+        csv_file = csv.writer(csvfile, delimiter = ',')
+        header = ['Date', 'Weight [kg]', 'Weight [lbs]']
+        csv_file.writerow(header)
+        for i in my_messages:
+            ex_value = [f'{i.date_val.month}-{i.date_val.day}-{i.date_val.year}', i.weight, i.weight * 2.20462]
+            csv_file.writerow(ex_value)
 
 def main():
     search_data, mail = authenticate()
-    email_data_extraction(search_data, mail)
-
+    my_messages = email_data_extraction(search_data, mail)
+    clean_data(my_messages)
+    file_name = 'weights.csv'
+    export_csv(my_messages, file_name)
     mail.logout()
 
 if __name__ == '__main__':
